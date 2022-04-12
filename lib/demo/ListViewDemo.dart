13db837b87b55c6f33d1089bb2105e5cd5d84489
胡冰洋 <850330609@qq.com>
2022-04-12 11:13:35 +0800

    1

---
+++ lib/demo/ListViewDemo.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class ListViewDemo extends StatelessWidget {
+  const ListViewDemo({Key? key}) : super(key: key);
+  // 列表项 处理
+  Widget _listItemBuilder(BuildContext context, int index) {
+    // return Text(posts[index].title);
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        Image.network(posts[index].imageUrl),
+        const SizedBox(height: 16.0),
+        Text(
+          posts[index].title,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        Text(
+          posts[index].author,
+          style: Theme.of(context).textTheme.subtitle2,
+        ),
+        const SizedBox(height: 16.0)
+      ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilder,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import 'demo/ListViewDemo.dart';
 
 void main() {
   // runApp(const Center(
@@ -23,8 +23,9 @@ class App extends StatelessWidget {
     // return Container(
     // );
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // 去掉 debug 标记
       home: const Home(),
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(primarySwatch: Colors.amber),
     );
   }
 }
@@ -32,41 +33,15 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
-  // 列表项 处理
-  Widget _listItemBuilder(BuildContext context, int index) {
-    // return Text(posts[index].title);
-    return Container(
-      color: Colors.white,
-      margin: const EdgeInsets.all(8.0),
-      child: Column(children: [
-        Image.network(posts[index].imageUrl),
-        const SizedBox(height: 16.0),
-        Text(
-          posts[index].title,
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-        Text(
-          posts[index].author,
-          style: Theme.of(context).textTheme.subtitle2,
-        ),
-        const SizedBox(height: 16.0)
-      ]),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: const Text('hello'),
-        elevation: 20.0, //阴影 0.0 去阴影
+        elevation: 8.0, //阴影 0.0 去阴影
       ),
-      // body: const Hello(),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listItemBuilder,
-      ),
+      body: const Hello(),
     );
   }
 }
@@ -82,7 +57,7 @@ class Hello extends StatelessWidget {
         textDirection: TextDirection.ltr,
         // 文字样式
         style: TextStyle(
-            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.yellow),
+            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.amber),
       ),
     );
   }
